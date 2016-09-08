c847d511e30401a1827b264563d8e563aa883859
hyb1996 <946994919@qq.com>
2016-09-08 21:51:24 +0800

    add a static file server for using livereload

---
+++ static file server/static_file_server.js
@@ -0,0 +1,52 @@
+
+var http = require("http");
+var server = http.createServer(staticFileServer);
+
+server.listen(80);
+console.log("Server is listening");
+
+
+var fs = require("fs");
+var url = require("url");
+var ROOT_DIR = "../";
+
+function staticFileServer(request, response){
+	if(request.url == "/"){
+		request.url = "/index.html";
+	}
+	var urlObj = url.parse(request.url);
+	fs.readFile(ROOT_DIR + urlObj.pathname, function(error, data){
+		if(error){
+			response.writeHead(404);
+			response.end("Page not found!");
+			console.log("File read error: url=%s path=%s", request.url, urlObj.pathname);
+			console.log("     error detail: " + error);
+			return;
+		}
+		var contentType = getContentType(urlObj.pathname);
+		if(contentType == null){
+			response.writeHead(200);
+		}else{
+			response.writeHead(200, {"Content-Type": contentType});
+		}
+		response.end(data);
+	});
+}
+
+var contentTypes = {
+	".html": "text/html",
+	".js": "text/javascript",
+	".css": "text/css",
+	".jpg": "image/jpeg",
+	".png": "image/png",
+	".gif": "image/gif"
+}
+
+function getContentType(path){
+	for(var key in contentTypes){
+		if(path.endsWith(key)){
+			return contentTypes[key];
+		}
+	}
+	return null;
+}
